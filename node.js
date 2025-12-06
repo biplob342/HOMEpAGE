387b1c251e056cfc04fe54e20f3a1aab2e37898a
biplob342 <ahmedbiplob540@gmail.com>
2025-12-06 12:08:14 +0600

    Update node.js

--- node.js
+++ node.js
@@ -3,7 +3,7 @@ let blb1 =document.getElementById("bulb1")
 let hd = document.getElementById("head")
 
 swth1.addEventListener("click", function(){
-    blb1.style.backgroundColor="blue";
     window.location.href="index2.html";
+
 
 })
